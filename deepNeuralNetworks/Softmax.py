08d462b62737d03c86c8c353efc65793fc5f4d40
Malathi Sankar <malathi.sankar@glassdoor.com>
2017-05-13 18:51:12 -0700

    Softmax

---
+++ deepNeuralNetworks/Softmax.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Write a function that takes as input a list of numbers, and returns
+# the list of values given by the softmax function.
+def softmax(L):
+    expOfL = np.exp(L)
+    sumOfExp = sum(expOfL)
+    ret = []
+    for i in expOfL:
+        ret.append(i/sumOfExp)
+    return ret
